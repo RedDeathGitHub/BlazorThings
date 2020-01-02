0ddf6d9ca68ba025cc0b8c6731ab3c5100f2e034
RedDeathGitHub <mateusz.zolnowski@gmail.com>
2020-01-02 08:26:37 +0100

    Add routing page

---
+++ BlazorRevealed/BlazorRevealed.Client/Pages/RoutingPage.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorRevealed.Client.Pages
+{
+    public class RoutingPageBase : ComponentBase
+    {
+        [Parameter]
+        public string Parameter { get; set; }
+    }
+}
